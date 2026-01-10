3d96e973961ba741def1aa1ed5d398a639bcc675
chinmay-s-patil <patil.chinmay3031@gmail.comclear>
2026-01-10 23:15:35 +0100

    Update 0.362

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    // use the separate PostCSS plugin package required by Tailwind v4+
+    '@tailwindcss/postcss': {},
+    autoprefixer: {}
+  }
 }
